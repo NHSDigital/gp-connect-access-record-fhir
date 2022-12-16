b732ff645eea4c0345e1c1639a4051924458e45d
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-12-16 12:06:41 +0000

    AMB-1359 add endpoints json

---
+++ tests/test_cache.py
@@ -0,0 +1,10 @@
+import requests
+import pytest
+from os import getenv
+
+
+@pytest.mark.debug
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
+def test_cache(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    resp = requests.get(f"{nhsd_apim_proxy_url}/", headers=nhsd_apim_auth_headers)
+    print(resp.json)
