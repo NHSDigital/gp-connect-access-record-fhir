9801e6aadfd4fc1b5b6398ad54c23c2444688af0
artronics <jalalhosseiny@gmail.com>
2023-02-02 23:53:55 +0000

    AMB-1386 add main.tf and add lock file

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
+    }
+  }
+  backend "s3" {
+    bucket = "nhsd-apim-gp-connect-pfs-access-record-terraform"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+  profile = "apim-dev"
+  default_tags {
+    tags = {
+      Project     = var.project
+      Environment = local.environment
+      Service     = var.service
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "eu-west-2"
+}
