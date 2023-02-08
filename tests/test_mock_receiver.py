8db0ff6dc474fdb4eb9b45d48c7d1869de942a98
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-02-08 13:48:12 +0000

    AMB-1385 add happy path test

--- tests/test_mock_receiver.py
+++ tests/test_mock_receiver.py
@@ -1,11 +1,13 @@
 import pytest
 import requests
 
+from .config import interaction_id
+
 
 @pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9"})
 @pytest.mark.debug
 def test_mock_receiver_happy_path(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    headers = {"Interaction-ID": "IN150016UK06"}
+    headers = {"Interaction-ID": interaction_id}
     headers.update(nhsd_apim_auth_headers)
 
     resp = requests.get(f"{nhsd_apim_proxy_url}/", headers=headers)
