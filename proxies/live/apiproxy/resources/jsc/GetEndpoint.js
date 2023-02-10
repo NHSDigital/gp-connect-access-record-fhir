d448e418980cb34f16a7dcc0046241e6812bcb2b
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-02-10 13:40:57 +0000

    AMB-1421 update proxy to use KVM instead of cache

--- proxies/live/apiproxy/resources/jsc/GetEndpoint.js
+++
@@ -1,5 +0,0 @@
-var endpoints = JSON.parse(context.getVariable("endpoints").content)
-var endpointKey = context.getVariable("endpointKey")
-
-var endpoint = endpoints[endpointKey]
-context.setVariable("fetchedEndpoint", endpoint)
