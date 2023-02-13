18ce54d27e7144a400b2124bf2ddfe6f3cd323d3
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-02-13 13:17:15 +0000

    AMB-1421 rename cache tests

--- tests/test_kvm.py
+++ tests/test_kvm.py
@@ -7,7 +7,7 @@ from .config import interaction_id
 
 
 @pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9"})
-def test_cache(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+def test_kvm(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     headers = {"Interaction-ID": interaction_id}
     headers.update(nhsd_apim_auth_headers)
 
@@ -17,7 +17,7 @@ def test_cache(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
 
 
 @pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9"})
-def test_cache_miss(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+def test_kvm_miss(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     headers = {"Interaction-ID": str(uuid.uuid4())}
     headers.update(nhsd_apim_auth_headers)
 
