86ab0486e49d57db7ecd0f1eb49e7a3bfeb25b1e
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-02-17 17:12:24 +0000

    AMB-1428: Tweaked curl output

--- scripts/remove_kvm_entry.sh
+++ scripts/remove_kvm_entry.sh
@@ -4,12 +4,14 @@
 KEYNAME=$1
 URL="https://api.enterprise.apigee.com/v1/organizations/nhsd-nonprod/environments/internal-dev/keyvaluemaps/gp-connect-access-record-endpoints-PR/entries/$KEYNAME"
 RESPONSE_CODE=$(curl -XGET -s -o /dev/null -w -H "Authorization: Bearer $APIGEE_ACCESS_TOKEN" $URL)
+echo $RESPONSE_CODE
 
 if [ $RESPONSE_CODE -eq "200" ]
     then
       # Delete PR KVM Entry
       URL="https://api.enterprise.apigee.com/v1/organizations/nhsd-nonprod/environments/internal-dev/keyvaluemaps/gp-connect-access-record-endpoints-PR/entries/$KEYNAME"
       RESPONSE_CODE=$(curl -s -o response.txt -w -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $APIGEE_ACCESS_TOKEN" $URL)
+      echo $RESPONSE_CODE
     else
       echo "The KVM or entry does not exist."
       exit 1
