df5bd5d44128120190576f0c3d477e385ebb76d1
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-02-20 11:44:55 +0000

    AMB-1428: Debug tweak

--- scripts/remove_kvm_entry.sh
+++ scripts/remove_kvm_entry.sh
@@ -6,13 +6,13 @@ URL="https://api.enterprise.apigee.com/v1/organizations/nhsd-nonprod/environment
 RESPONSE_CODE=$(curl -XGET -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $APIGEE_ACCESS_TOKEN" $URL)
 echo $RESPONSE_CODE
 
-if [ $RESPONSE_CODE -eq "200" ]
-    then
-      # Delete PR KVM Entry
-      URL="https://api.enterprise.apigee.com/v1/organizations/nhsd-nonprod/environments/internal-dev/keyvaluemaps/gp-connect-access-record-endpoints-PR/entries/$KEYNAME"
-      RESPONSE_CODE=$(curl -s -o response.txt -w "%{http_code}" -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $APIGEE_ACCESS_TOKEN" $URL)
-      echo $RESPONSE_CODE
-    else
-      echo "The KVM or entry does not exist."
-      exit 1
-fi
+#if [ $RESPONSE_CODE -eq "200" ]
+#    then
+#      # Delete PR KVM Entry
+#      URL="https://api.enterprise.apigee.com/v1/organizations/nhsd-nonprod/environments/internal-dev/keyvaluemaps/gp-connect-access-record-endpoints-PR/entries/$KEYNAME"
+#      RESPONSE_CODE=$(curl -s -o response.txt -w "%{http_code}" -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $APIGEE_ACCESS_TOKEN" $URL)
+#      echo $RESPONSE_CODE
+#    else
+#      echo "The KVM or entry does not exist."
+#      exit 1
+#fi
