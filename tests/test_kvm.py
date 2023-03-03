f8ee88b36fbf5013f11fce479bb3586fde82c837
almas-liaqat <Almas.liaqat@accenture.com>
2023-03-03 11:24:50 +0000

    Pull from master

--- tests/test_kvm.py
+++ tests/test_kvm.py
@@ -4,7 +4,7 @@ import pytest
 import requests
 
 
-@pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9"})
+@pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9", "login_form": {"username": "9912003071"}})
 def test_kvm_miss(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     headers = {"Interaction-ID": str(uuid.uuid4())}
     headers.update(nhsd_apim_auth_headers)
--- tests/test_mock_receiver.py
+++ tests/test_mock_receiver.py
@@ -9,7 +9,7 @@ from .config import interaction_id
 @pytest.mark.debug
 @pytest.mark.integration
 @pytest.mark.user_restricted_separate_nhs_login
-@pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9"})
+@pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9", "login_form": {"username": "9912003071"}})
 def test_mock_receiver_patient_record_path(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     headers = {
               "Interaction-ID": interaction_id, "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068",
