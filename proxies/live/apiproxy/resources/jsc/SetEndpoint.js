7a885f0ba060cf1f552c199bac7b1c0168b64167
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-03-13 10:47:58 +0000

    AMB-1439: Added `endpoint_no_protocol` var

--- proxies/live/apiproxy/resources/jsc/SetEndpoint.js
+++ proxies/live/apiproxy/resources/jsc/SetEndpoint.js
@@ -2,6 +2,9 @@ const queryString = context.getVariable("request.querystring")
 const pathSuffix = context.getVariable("proxy.pathsuffix")
 
 const endpoint = context.getVariable("endpoint")
+// Needed so we can pass the endpoint to a service callout
+const endpoint_no_protocol = endpoint.split("://")[1]
+context.setVariable("endpoint_no_protocol", endpoint_no_protocol)
 
 if (endpoint) {
   url = endpoint + pathSuffix
