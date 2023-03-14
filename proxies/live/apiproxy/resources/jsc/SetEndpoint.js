7ad208a937fb32953dfbce4016955fcd138b5627
almas-liaqat <Almas.liaqat@accenture.com>
2023-03-14 13:50:16 +0000

    Added config from KVM in apigee

--- proxies/live/apiproxy/resources/jsc/SetEndpoint.js
+++ proxies/live/apiproxy/resources/jsc/SetEndpoint.js
@@ -24,3 +24,51 @@ if (endpoint) {
 } else {
   context.setVariable("endpointNotFound", true)
 }
+
+
+
+
+function json_tryparse(raw) {
+  try {
+      return JSON.parse(raw);
+  }
+  catch (e) {
+      return raw;
+  }
+}
+
+print("==========endpointConfig==================")
+var endpointConfig=context.getVariable("private.config")
+print(endpointConfig)
+
+
+const parsedEndpointConfig = json_tryparse(endpointConfig)
+
+print("==========parsedEndpointConfig===================")
+print(parsedEndpointConfig)
+
+print("==========Oauth2==================")
+Oauth2Config=parsedEndpointConfig["oauth2"]
+print(Oauth2Config)
+
+
+print("==========clientid==================")
+context.setVariable("client_id_config",Oauth2Config["client_id"])
+context.setVariable("kid_config",Oauth2Config["kid"])
+context.setVariable("audience_config",Oauth2Config["audience"])
+context.setVariable("subject_issuer_config",Oauth2Config["subject_issuer"])
+context.setVariable("private2.jwt",Oauth2Config["private_key"])
+
+print(context.getVariable("client_id_config"))
+print(context.getVariable("kid_config"))
+print(context.getVariable("audience_config"))
+print(context.getVariable("subject_issuer_config"))
+print(context.getVariable("private2.jwt")) 
+
+
+var fs = require('fs');
+var path = context.getVariable('private2.jwt');
+var content = fs.readFileSync(path, 'utf8');
+context.setVariable('fileContent', content);
+print("==========PrivateKEy Content==================")
+print(context.getVariable('fileContent'));
