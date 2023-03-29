595f7b161d8764a1436c0802cb830f3236dd6d26
almas-liaqat <Almas.liaqat@accenture.com>
2023-03-29 15:24:21 +0100

    added test for ecs task deplpoyment

---
+++ tests/test_ecs_deployment.py
@@ -0,0 +1,10 @@
+import requests
+import pytest
+from os import getenv
+
+@pytest.mark.clouddeploymenttest
+def test_ping_endpoint():
+    domain_name=getenv('CLOUD_DOMAIN_NAME')
+    url = domain_name
+    response = requests.get(url)
+    assert response.status_code == 200
