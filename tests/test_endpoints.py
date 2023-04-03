37d20bd38b5a38025d224f84d2af17908ad0f3ac
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-04-03 15:24:19 +0100

    AMB-1439: Updated missing headers

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -91,11 +91,20 @@ def test_auth_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     {
         "access": "patient",
         "level": "P9",
-        "login_form": {"username": "9912003071"},
+        "login_form": {"username": "9912003071"}
     }
 )
 def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    headers = {"Interaction-ID": interaction_id}
+    headers = {
+        "Interaction-ID": interaction_id,
+        "accept": "application/fhir+json",
+        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
+        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068",
+        "Ssp-TraceID": "09a01679-2564-0fb4-5129-aecc81ea2706",
+        "Ssp-From": "200000000359",
+        "Ssp-To": "918999198738",
+        "Ssp-PatientInteration": "urn:nhs:names:services:gpconnect:documents:fhir:rest:search:patient-1",
+    }
     headers.update(nhsd_apim_auth_headers)
 
     resp = requests.get(f"{nhsd_apim_proxy_url}/documents/Patient/9000000009", headers=headers)
