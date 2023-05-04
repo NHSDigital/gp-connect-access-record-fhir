9233a735ce0b4d5279cf66098788d74c6a53e99a
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-05-04 16:23:32 +0100

    AMB-1510 add temporary external file to the spec to test prism

--- tests/test_mock_provider.py
+++ tests/test_mock_provider.py
@@ -5,6 +5,7 @@ import requests
 @pytest.mark.smoketest
 @pytest.mark.auth
 @pytest.mark.integration
+@pytest.mark.debug
 @pytest.mark.user_restricted_separate_nhs_login
 @pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9", "login_form": {"username": "9912003071"}})
 def test_mock_receiver_patient_record_path(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
