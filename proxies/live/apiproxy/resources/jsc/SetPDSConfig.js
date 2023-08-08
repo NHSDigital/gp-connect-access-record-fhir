d330ec37c17bc19fb3e3defdf2567d7f8d810375
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-08-08 13:43:22 +0100

    AMB-1474 extract ODS code from PDS service callout

--- proxies/live/apiproxy/resources/jsc/SetPDSConfig.js
+++ proxies/live/apiproxy/resources/jsc/SetPDSConfig.js
@@ -1,4 +1,5 @@
 var nhs_number = context.getVariable("jwt.DecodeJWT.DecodeIDToken.claim.nhs_number")
+var x_request_id = context.getVariable("request.header.X-Request-ID")
 var PDSConfig=context.getVariable("private.pds_config")
 const parsedPDSConfig = JSON.parse(PDSConfig)
 
@@ -7,3 +8,5 @@ Oauth2Config=parsedPDSConfig["oauth2"]
 context.setVariable("pds_client_id_config",Oauth2Config["client_id"])
 context.setVariable("pds_kid_config",Oauth2Config["kid"])
 context.setVariable("pds_audience_config",Oauth2Config["audience"])
+context.setVariable("nhs_number",nhs_number)
+context.setVariable("X-x_request_id-ID",x_request_id)
