ca474045fc810238888fb4b3ceeb172c316d3f74
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-08-08 15:44:53 +0100

    AMB-1474 update tests with new test user

--- tests/test_kvm.py
+++ tests/test_kvm.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 
 
-@pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9", "login_form": {"username": "9912003071"}})
+@pytest.mark.nhsd_apim_authorization({"access": "patient", "level": "P9", "login_form": {"username": "9449305552"}})
 def test_kvm_miss(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
 
     resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
