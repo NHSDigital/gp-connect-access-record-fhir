8b0837dce2b8c44d2e2ca407ad6020e94c5916d2
raghuram grandhi <raghu.ramgrandhi1@nhs.net>
2025-12-23 11:16:29 +0000

    removed js.

--- proxies/live/apiproxy/resources/jsc/ValidateNHSNumber.js
+++ proxies/live/apiproxy/resources/jsc/ValidateNHSNumber.js
@@ -3,10 +3,7 @@ function decodeJWT(token) {
     if (parts.length !== 3) {
         throw new Error("Invalid JWT format");
     }
-    var header = JSON.parse(base64UrlDecode(parts[0]));
-    var payload = JSON.parse(base64UrlDecode(parts[1]));
-    var signature = parts[2];
-    return { header: header, payload: payload, signature: signature };
+    
 }
 
 var outerDecoded = decodeJWT("hello");
