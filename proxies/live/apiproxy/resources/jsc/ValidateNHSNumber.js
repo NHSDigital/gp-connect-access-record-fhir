c345ae3815f5de15fa60f02e018dbce2436b80a1
Raghuram Grandhi <raghu.ramgrandhi1@nhs.net>
2026-08-27 16:37:10 +0100

    changes related to composite to id implementation.

--- proxies/live/apiproxy/resources/jsc/ValidateNHSNumber.js
+++ proxies/live/apiproxy/resources/jsc/ValidateNHSNumber.js
@@ -1,66 +1,77 @@
-// ---- Debug (trace only) ----
-print("nhsd.actor.nhs_number   : " + context.getVariable("nhsd.actor.nhs_number"));
-print("nhsd.subject.nhs_number : " + context.getVariable("nhsd.subject.nhs_number"));
+const subNHS = context.getVariable("nhsd.subject.nhs_number");
 
-var topClaims = context.getVariable('jwt.actClaim'); // JSON string if VerifyJWT set <OutputClaimVariables/>
-print("topClaims  :" + topClaims)
-var claims = topClaims ? JSON.parse(topClaims) : {};
-print("claims : " + claims.act);
-var hasAct = !!claims.act;
-print("hasAct : " + hasAct);
-
-if (hasAct) {
-    // ---- Subject NHS number (from composite ID shared flow) ----
-    var subNHS = context.getVariable("nhsd.subject.nhs_number");
-    subNHS = String(subNHS).trim();
-
-    // ---- HTTP Verb ----
-    var httpverb = context.getVariable("request.verb");
+if (subNHS) {
+    const httpverb = String(context.getVariable("request.verb")).toLowerCase();
+    const errorObject = { error: 'invalid_token', errorDescription: "NHS ID could not be validated", statusCode: 401, reasonPhrase: "Unauthorized" };
     var requestNHS = null;
+    if (httpverb === 'get') {
+        const queryParams = context.getVariable("request.querystring");
+        var normalizedParams = {};
 
-    if (httpverb === 'GET') {
-        var queryNHSNumber = context.getVariable("request.queryparam.patientNHSNumber");
+        if (queryParams) {
+            var pairs = queryParams.split("&");
+            pairs.forEach(function(pair) {
+                var parts = pair.split("=");
+                if (parts.length === 2) {
+                    normalizedParams[parts[0].toLowerCase()] = decodeURIComponent(parts[1]);
+                }
+            });
+        }
+        const queryNHSNumber = normalizedParams["patientnhsnumber"];
         if (queryNHSNumber) {
             requestNHS = String(queryNHSNumber).trim();
-            print("NHS from query: " + requestNHS);
         }
         else {
-            var pathSuffix = context.getVariable("proxy.pathsuffix");
+            const pathSuffix = context.getVariable("proxy.pathsuffix");
             if (pathSuffix) {
                 var parts = pathSuffix.split('/').filter(Boolean);
                 requestNHS = parts.length ? parts[parts.length - 1] : null;
-                print("NHS from route: " + requestNHS);
             }
         }
     }
-    else if (httpverb === 'POST') {
-        var reqContent = context.getVariable("request.content");
+    else if (httpverb === 'post') {
+        const reqContent = context.getVariable("request.content");
+        var validJSON = 'true';
         if (!reqContent) {
-            //raiseNHSFault("Request body missing");
-            //return;
+            validJSON = 'false';
+            context.setVariable('trigger.raiseNHSNumberFault', true);
+            context.setVariable('validation.errorMessage', errorObject.error);
+            context.setVariable('validation.errorDescription', errorObject.errorDescription);
+            context.setVariable('validation.statusCode', errorObject.statusCode);
+            context.setVariable('validation.reasonPhrase', errorObject.reasonPhrase);
         }
         var jsonContent;
         try {
             jsonContent = JSON.parse(reqContent);
         } catch (e) {
-            //raiseNHSFault("Invalid JSON payload");
-            //return;
+            validJSON = 'false';
+            context.setVariable('trigger.raiseNHSNumberFault', true);
+            context.setVariable('validation.errorMessage', errorObject.error);
+            context.setVariable('validation.errorDescription', errorObject.errorDescription);
+            context.setVariable('validation.statusCode', errorObject.statusCode);
+            context.setVariable('validation.reasonPhrase', errorObject.reasonPhrase);
         }
         if (!jsonContent.parameter || !Array.isArray(jsonContent.parameter)) {
-            //raiseNHSFault("Invalid Parameters structure");
-            //return;
+            validJSON = 'false';
+            context.setVariable('trigger.raiseNHSNumberFault', true);
+            context.setVariable('validation.errorMessage', errorObject.error);
+            context.setVariable('validation.errorDescription', errorObject.errorDescription);
+            context.setVariable('validation.statusCode', errorObject.statusCode);
+            context.setVariable('validation.reasonPhrase', errorObject.reasonPhrase);
         }
-        for (var i = 0; i < jsonContent.parameter.length; i++) {
-            var p = jsonContent.parameter[i];
-            if ( p.name === "patientNHSNumber" && p.valueIdentifier && p.valueIdentifier.value ) {
-                requestNHS = String(p.valueIdentifier.value).trim();
-                break;
+        if(validJSON === 'true') {
+            for (var i = 0; i < jsonContent.parameter.length; i++) {
+                var p = jsonContent.parameter[i];
+                if ( p.name === "patientNHSNumber" && p.valueIdentifier && p.valueIdentifier.value ) {
+                    requestNHS = String(p.valueIdentifier.value).trim();
+                    break;
+                }
             }
         }
     }
-    if (requestNHS && requestNHS !== subNHS) {
+    requestNHS = requestNHS.trim();
+    if(requestNHS !== subNHS) {
         context.setVariable('trigger.raiseNHSNumberFault', true);
-        var errorObject = { error: 'invalid_token', errorDescription: "NHS ID could not be validated", statusCode: 401, reasonPhrase: "Unauthorized" };
         context.setVariable('validation.errorMessage', errorObject.error);
         context.setVariable('validation.errorDescription', errorObject.errorDescription);
         context.setVariable('validation.statusCode', errorObject.statusCode);
